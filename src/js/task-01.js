144e7b862c131656d233804f493492af72ba56fc
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-01-09 23:54:29 +0200

    task-1-first-try

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+const itemNumberList = document.querySelectorAll('li.item');
+// console.log(itemNumberList);
+
+const numberOfCategories = itemNumberList.length;
+console.log(numberOfCategories);
+
+const categoryTitleOne = document.querySelector('h2').textContent;
+console.log(categoryTitleOne);
+
+const categoryItems = document.querySelector('li').textContent;
+console.log(categoryItems);
+
+const categoryItemsNumberOne = categoryItems.length;
+console.log(categoryItemsNumberOne);
