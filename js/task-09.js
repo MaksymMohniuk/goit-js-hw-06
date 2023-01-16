5bda316e3bcf2c2020e80c831c1fdb8d3144f6d4
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-01-16 22:33:55 +0200

    task-9-finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const color = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  color.textContent = document.body.style.backgroundColor;
+});
+
