a0b2fb3ba07e24a73d6c137b1a4ca174b727523e
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-01-20 00:19:44 +0200

    udate-tasks-6-10

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,11 @@
 const textInput = document.getElementById('validation-input');
 const textLength = Number(textInput.dataset.length);
 
+
 textInput.addEventListener('blur', (event) => {
+    textInput.classList.add('valid');
 if (event.currentTarget.value.length !== textLength) {
-    textInput.classList.add('invalid');
+    textInput.classList.replace('valid', 'invalid');
 } else {
     textInput.classList.replace('invalid', 'valid');
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector('#controls input');
+console.log(input);
+const btnCreate = document.querySelector('#controls button[data-create]');
+console.log(btnCreate);
+const btnDestroy = document.querySelector('#controls button[data-destroy]');
+console.log(btnDestroy);
+const container = document.getElementById('boxes');
+console.log(container);
+
+
